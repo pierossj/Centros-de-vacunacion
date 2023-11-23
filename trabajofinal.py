0a4790f451ec80016a5e3942d9fd4d42dbc2fc3f
pierossj <115940841+pierossj@users.noreply.github.com>
2023-11-23 15:13:50 -0500

    Update trabajofinal.py

--- trabajofinal.py
+++ trabajofinal.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+
 
 st.set_page_config(page_title="Centros de vacunacion") 
 st.header('Centros de Vacunación en el Perú del 2020-21') 
